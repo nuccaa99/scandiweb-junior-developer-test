7c14a16159c403f77243a3f32a0d4adcff962ddf
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-08-24 17:16:08 +0400

    impoved favs page styling

--- src/pages/Favs.jsx
+++ src/pages/Favs.jsx
@@ -13,18 +13,20 @@ function Favs() {
   return (
     <div className="fav_items_container">
       <h2 className='cart_items_title'>Favs</h2>
-      {favItems.length ? (
-        favItems.map((item) => {
-          return (
-            <Product product={item} key={item.id} location={currentPath} />
-          );
-        })
-      ) : (
-        <div className="empty_cart">
-          <img src={emptyBasket} alt="empty cart" className="empty_cart_img" />
-          <p>You don't have any favourites</p>
-        </div>
-      )}
+      <div className='fav_items_wrapper'>
+        {favItems.length ? (
+          favItems.map((item) => {
+            return (
+              <Product product={item} key={item.id} location={currentPath} />
+            );
+          })
+        ) : (
+          <div className="empty_cart">
+            <img src={emptyBasket} alt="empty cart" className="empty_cart_img" />
+            <p>You don't have any favourites</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
